9c606f0d5952a7f72f53d392fe43b53636327296
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2024-11-04 12:45:05 +0800

    adding and customizing shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <main>
-      <Button>Click Me</Button>
-      <p className="text-blue-500 font-semibold">Evan</p>
+    <main className="flex gap-1">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
+      <Input />
     </main>
   );
 }
