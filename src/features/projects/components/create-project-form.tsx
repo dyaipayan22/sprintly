193329ec67c54bc399df34743a2252233a61716d
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2025-01-21 19:33:20 +0800

    adding new functionalities - added update project feature - added delete project feature

--- src/features/projects/components/create-project-form.tsx
+++ src/features/projects/components/create-project-form.tsx
@@ -58,7 +58,7 @@ export const CreateProjectForm = ({ onCancel }: CreateProjectFormProps) => {
       {
         onSuccess: ({ data }) => {
           form.reset();
-          // TODO: Redirect to project screen
+          router.push(`/workspaces/${workspaceId}/projects/${data.$id}`);
         },
       }
     );
