7234f02e668791c383716fe0640f3863c3c7b0df
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2025-02-03 22:58:51 +0800

    building a data table

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -11,6 +11,9 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { DataFilters } from "./data-filters";
 
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+
 import { useGetTasks } from "../api/use-get-tasks";
 import { useCreateTaskModel } from "../hooks/use-create-task-modal";
 import { useTaskFilters } from "../hooks/use-task-filters";
@@ -64,7 +67,7 @@ export const TaskViewSwitcher = () => {
         ) : (
           <>
             <TabsContent value="table" className="mt-0">
-              {JSON.stringify(tasks)}
+              <DataTable columns={columns} data={tasks?.documents ?? []} />
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
               {JSON.stringify(tasks)}
--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -11,7 +11,7 @@ import { getMember } from "@/features/members/utils";
 import { Project } from "@/features/projects/types";
 
 import { createTaskSchema } from "../schemas";
-import { TaskStatus } from "../types";
+import { Task, TaskStatus } from "../types";
 
 const app = new Hono()
   .get(
@@ -76,7 +76,7 @@ const app = new Hono()
         query.push(Query.search("name", search));
       }
 
-      const tasks = await databases.listDocuments(DATABASE_ID, TASKS_ID, query);
+      const tasks = await databases.listDocuments<Task>(DATABASE_ID, TASKS_ID, query);
 
       const projectIds = tasks.documents.map((task) => task.projectId);
       const assigneeIds = tasks.documents.map((task) => task.assigneeId);
