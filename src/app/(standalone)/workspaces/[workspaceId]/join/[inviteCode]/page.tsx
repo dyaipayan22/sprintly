4531e25ba3fe6d0f62ec0752ed7317e015ac3da7
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2025-02-11 22:41:47 +0800

    refactoring server components - refactored the scenario where server components are not suitable

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,27 +1,14 @@
-import { getCurrent } from "@/features/auth/queries";
-import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
-import { getWorkspaceInfo } from "@/features/workspaces/queries";
 import { redirect } from "next/navigation";
 
-interface WorkspaceIdJoinPageProps {
-  params: { workspaceId: string };
-}
+import { getCurrent } from "@/features/auth/queries";
 
-const WorkspaceIdJoinPage = async ({ params }: WorkspaceIdJoinPageProps) => {
+import { WorkspaceIdJoinClient } from "./client";
+
+const WorkspaceIdJoinPage = async () => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
-  const initialValues = await getWorkspaceInfo({
-    workspaceId: params.workspaceId,
-  });
-
-  if (!initialValues) redirect("/");
-
-  return (
-    <div className="w-full lg:max-w-xl">
-      <JoinWorkspaceForm initialValues={initialValues} />
-    </div>
-  );
+  return <WorkspaceIdJoinClient />;
 };
 
 export default WorkspaceIdJoinPage;
